00db48adc76648c46760ada6e481e0803d1dd4f1
Clark Blas <clarkblasyoh@gmail.com>
2016-10-13 07:35:03 +0800

    Initial Commit puts sir ed code

---
+++ forProj/src/forproj/ForProj.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forproj;
+
+/**
+ *
+ * @author cdblas
+ */
+public class ForProj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Clark is Here
+        // Stay behind Clark
+        System.out.println("Hi Sir!");
+    }
+    
+}

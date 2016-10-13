b97bde8c0aa2a009e58efa663467fa7b9a2215fc
Levi Paul Bretaña <bretanalevipaul@gmail.com>
2016-10-13 08:12:43 +0800

    Input sir ed code.

--- Iteu333a-project/src/iteu333a/project/Iteu333aProject.java
+++ Iteu333a-project/src/iteu333a/project/Iteu333aProject.java
@@ -1,21 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package iteu333a.project;
+package Iteu333a.project;
 
-/**
- *
- * @author easalinas
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Iteu333aProject {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException, IOException {
+        Scanner sc = new Scanner(new FileReader("input.txt"));
+
+        //reads the file "input.txt"
+        PrintWriter writer = new PrintWriter("Test.java", "UTF-8");
+
+        while (sc.hasNext()) {
+            String str = sc.nextLine();
+            //replaces kung with if
+            str = str.replaceAll("kung", "if");
+            str = str.replaceAll("labas", "out");
+            str = str.replaceAll("iprint", "println");
+
+            writer.println(str);
+        }
+        writer.close();
+        List cmdAndArgs = Arrays.asList("cmd", "/c", "run.bat");
+        File dir = new File(System.getProperty("user.dir"));
+
+        ProcessBuilder pb = new ProcessBuilder(cmdAndArgs);
+        pb.directory(dir);
+        Process p = pb.start();
     }
-    
 }

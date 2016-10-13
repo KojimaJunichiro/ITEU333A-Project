2e00e4dd5e6726a641b97d6e7f54f078fb348737
bretanalevi <bretanalevipaul@gmail.com>
2016-10-14 02:43:53 +0800

    SYNTAX ERROR CHECKING

--- Iteu333a-project/src/iteu333a/project/Iteu333aProject.java
+++ Iteu333a-project/src/iteu333a/project/Iteu333aProject.java
@@ -9,23 +9,41 @@ import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Iteu333aProject {
 
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException, IOException {
         Scanner sc = new Scanner(new FileReader("input.txt"));
 
+        
+        int lineCount = 0;
         //reads the file "input.txt"
         PrintWriter writer = new PrintWriter("Test.java", "UTF-8");
 
-        while (sc.hasNext()) {
+        while (sc.hasNext()) 
+        {
             String str = sc.nextLine();
             //replaces kung with if
             str = str.replaceAll("kung", "if");
             str = str.replaceAll("labas", "out");
             str = str.replaceAll("iprint", "println");
+            lineCount++;
+            
+            
+            
+              if (str.trim().endsWith(";")||str.trim().endsWith("{")||str.trim().endsWith("}")||str.trim().isEmpty())
+                {
 
-            writer.println(str);
+                }
+                else
+                {
+                    System.out.println("Error at line" + " " + lineCount);                   
+                }
+                writer.println(str);
+            
+            
         }
         writer.close();
         List cmdAndArgs = Arrays.asList("cmd", "/c", "run.bat");

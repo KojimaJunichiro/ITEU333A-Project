c07a8db6aeb5b8442871fd8be7aa583f04d56174
bretanalevi <bretanalevipaul@gmail.com>
2016-10-19 04:25:09 +0800

    SYNTAX ERROR CHECKING ( INT , STRING )

---
+++ Iteu333aproject/Test.java
@@ -0,0 +1,16 @@
+//public class Test {
+    //public static void main(String[] args){
+        int i = 0;
+	int levipaul =0;
+
+String levi = "levipaul;
+String $levi = "levipaul";
+	String levi = "levipaul";
+String levi="levipaul";
+		if(i==1){
+			System.out.println("kumusta mundo");
+			levi paul
+
+		//}
+	//}
+//}
